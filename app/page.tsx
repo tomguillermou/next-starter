b4f781d4105b75df3bdc338dd34bf037e426facd
tomguillermou <tomguillermou@gmail.com>
2025-02-02 08:50:44 +0100

    Install DaisyUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="p-12 text-center text-xl">Next.js + Prisma Starter 🚀</h1>
+    <main className="flex h-screen flex-col items-center justify-center gap-12">
+      <h1 className="text-2xl font-bold">
+        Next.js / Prisma / DaisyUI Starter 🚀
+      </h1>
+      <div className="flex gap-4">
+        <Link
+          className="link link-hover"
+          href="https://nextjs.org/docs"
+          target="_blank"
+        >
+          Next.js
+        </Link>
+        <Link
+          className="link link-hover"
+          href="https://www.prisma.io/docs/orm"
+          target="_blank"
+        >
+          Prisma
+        </Link>
+        <Link
+          className="link link-hover"
+          href="https://daisyui.com/docs"
+          target="_blank"
+        >
+          DaisyUI
+        </Link>
+      </div>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -14,5 +13,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require('daisyui')],
 } satisfies Config
